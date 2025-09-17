5c93de4d9024f74e051f7410dcb3c94b2a97b4dd
Saintmocha01 <mattmocha01@gmail.com>
2025-09-17 14:29:07 +0300

    Added SQL answers for assignment

---
+++ answers.sql
@@ -0,0 +1,15 @@
+-- Question 1 🗑️
+-- Drop an index named IdxPhone from customers table
+DROP INDEX IdxPhone ON customers;
+
+-- Question 2 👤
+-- Create a user named bob with the password 'S$cu3r3!' restricted to localhost
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3 🔑
+-- Grant INSERT privilege to bob on the salesDB database
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Question 4 🔐
+-- Change the password for bob to 'P$55!23'
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
